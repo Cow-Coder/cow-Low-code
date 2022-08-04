26ad3f412f14d0c611a172a51725d8799209808a
Eric Moore <EricGittaMoore@duck.com>
2022-08-04 14:53:02 +0800

    fix: 解决tailwind覆盖el-button样式 style: 使用prettier格式化css

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,13 @@ import router from "./router";
 import piniaPersist from "pinia-plugin-persist";
 import storeReset from "@/stores/plugins/storeReset";
 import "@/assets/tailwind.css";
-// import ElementPlus from "element-plus";
-// import "element-plus/dist/index.css";
-// import zhCn from "element-plus/es/locale/lang/zh-cn";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
-// import Vant from "vant";
-// import "vant/lib/index.css";
 
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
-// app.use(ElementPlus, {
-//   locale: zhCn,
-// });
-// app.use(Vant);
+
 app.use(router);
 app.use(createPinia().use(piniaPersist).use(storeReset));
 app.mount("#app");
