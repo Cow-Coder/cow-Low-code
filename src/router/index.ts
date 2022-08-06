bc39484a999541ca017aaf8611eed2bb06f6021a
JamesCurtis <49338067+james-curtis@users.noreply.github.com>
2022-08-06 20:41:45 +0800

    fix(codePanel): 第一次进入时未格式化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import codePanel from '@/components/codePanel/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,13 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-      children: [
-        {
-          path: '',
-          name: 'homeCode',
-          component: codePanel,
-        },
-      ],
     },
   ],
 })
