20622491b069edd0f1c0bb9fd9041725ffcba1dd
Eric Moore <EricGittaMoore@duck.com>
2022-08-09 11:53:57 +0800

    refactor: 统一DRAGGABLE_GROUP_NAME常量

--- src/views/edit-panel/config.ts
+++ src/views/edit-panel/config.ts
@@ -1,8 +1,9 @@
 import type { Draggable } from '@/components/base-ui/kzy-draggable/types'
+import { DRAGGABLE_GROUP_NAME } from '@/constant'
 
 export const config: Draggable = {
   draggableProp: {
-    group: { name: 'library' },
+    group: { name: DRAGGABLE_GROUP_NAME },
     itemKey: 'id',
     disabled: false,
   },
