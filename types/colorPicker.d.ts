7036438300abacf0435377ece7bfdd4797f118ef
20empty <2625502633@qq.com>
2022-08-14 00:18:01 +0800

    fix: 规整文件命名规范

--- types/colorPicker.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'colorpicker-v3' {
-  export default class ColorPicker {
-    constructor(el: HTMLElement, options: any)
-    destroy(): void
-    getColor(): string
-    setColor(color: string): void
-  }
-}
