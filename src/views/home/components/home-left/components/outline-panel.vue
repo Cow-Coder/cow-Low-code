46325e39cdddac7dd8ab27265ca20398214025d8
JamesCurtis <49338067+james-curtis@users.noreply.github.com>
2022-08-17 11:28:52 +0800

    fix: 类型导入错误NodeDropType

--- src/views/home/components/home-left/components/outline-panel.vue
+++ src/views/home/components/home-left/components/outline-panel.vue
@@ -9,10 +9,11 @@
         :allow-drop="allowDrop"
         :data="outline"
         highlight-current
-        draggable
+        :draggable="true"
         default-expand-all
         node-key="id"
         :filter-node-method="filterNode"
+        empty-text="暂无内容"
         @node-drop="handleDrop"
       />
     </div>
@@ -22,7 +23,7 @@
 <script lang="ts" setup>
 import { ElTree } from 'element-plus'
 import type Node from 'element-plus/es/components/tree/src/model/node'
-import type { DropType } from 'element-plus/es/components/tree/src/tree.type'
+import type { NodeDropType } from 'element-plus/es/components/tree/src/tree.type'
 import type { TreeData } from '@/utils/map-schemes-2-outline'
 import { useCodeStore } from '@/stores/code'
 import { mapSchemes2Outline } from '@/utils/map-schemes-2-outline'
@@ -38,7 +39,7 @@ const outline = computed(() => mapSchemes2Outline(outlineData.value))
 
 //TODO: 嵌套大纲
 // 是否允许放入
-const allowDrop = (draggingNode: Node, dropNode: Node, type: DropType) => type !== 'inner'
+const allowDrop = (draggingNode: Node, dropNode: Node, type: NodeDropType) => type !== 'inner'
 const filterText = ref('')
 const treeRef = ref<InstanceType<typeof ElTree>>()
 
