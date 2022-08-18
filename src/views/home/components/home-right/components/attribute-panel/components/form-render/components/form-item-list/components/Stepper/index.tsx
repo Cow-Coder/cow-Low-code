d7fdfc99b5524b8fb2bd07c80747083e92ab1c9d
20empty <2625502633@qq.com>
2022-08-19 00:58:10 +0800

    feat(library): 添加步进器表单渲染，增加圆形进度条物料

---
+++ src/views/home/components/home-right/components/attribute-panel/components/form-render/components/form-item-list/components/Stepper/index.tsx
@@ -0,0 +1,40 @@
+import { ElButton, ElInput } from 'element-plus'
+import $style from './index.module.scss'
+import type { PropType } from 'vue'
+export const Stepper = defineComponent({
+  name: 'Stepper',
+  props: {
+    modelValue: {
+      required: false,
+      type: String as PropType<string>,
+    },
+  },
+  emits: ['update:modelValue'],
+
+  setup(props, { emit }) {
+    const tipsVal = useVModel(props, 'modelValue', emit)
+    const increase = () => {
+      if (tipsVal.value == undefined) {
+        tipsVal.value = '1'
+      } else {
+        tipsVal.value = (Number.parseInt(tipsVal.value) + 1).toString()
+      }
+    }
+    const decrease = () => {
+      if (tipsVal.value == undefined) {
+        tipsVal.value = '1'
+      } else {
+        tipsVal.value = (Number.parseInt(tipsVal.value) - 1).toString()
+      }
+    }
+    return () => (
+      <>
+        <div class={[$style.stepper__inputGroup]}>
+          <ElButton onClick={decrease}>-</ElButton>
+          <ElInput v-model={tipsVal.value}></ElInput>
+          <ElButton onClick={increase}>+</ElButton>
+        </div>
+      </>
+    )
+  },
+})
