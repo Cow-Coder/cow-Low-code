df236801cec7785e9426b8fb4e6a783161cc1834
Eric Moore <EricGittaMoore@duck.com>
2022-08-19 19:50:09 +0800

    feat(docs): 首页截图预览

---
+++ docs/.vitepress/theme/components/zoom-img.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import { withBase } from 'vitepress'
+import mediumZoom from 'medium-zoom'
+
+defineProps({
+  src: String,
+})
+
+const imgRef = ref(null)
+
+onMounted(() => {
+  mediumZoom(imgRef.value, { background: 'var(--vp-c-bg)' })
+})
+</script>
+
+<template>
+  <p>
+    <img ref="imgRef" :src="withBase(src)" />
+  </p>
+</template>
+
+<style>
+.medium-zoom-overlay {
+  z-index: 200;
+}
+
+.medium-zoom-image {
+  z-index: 9;
+}
+
+.medium-zoom-image--opened {
+  z-index: 201;
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,21 @@
+import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import './styles/vars.css'
 import './styles/vitepress.css'
 import './custom.css'
+import HomePreview from './components/home-preview.vue'
+import ZoomImg from './components/zoom-img.vue'
+import CustomerEvaluate from './components/customer-evaluate.vue'
 
-export default Theme
+export default {
+  ...Theme,
+  Layout() {
+    return h(Theme.Layout, null, {
+      'home-features-after': () => h(HomePreview),
+    })
+  },
+  enhanceApp({ app }) {
+    app.component('ZoomImg', ZoomImg)
+    app.component('CustomerEvaluate', CustomerEvaluate)
+  },
+}
