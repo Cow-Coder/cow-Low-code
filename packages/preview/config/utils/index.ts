7d5d6ebd72031f327811f5338cb04a826d48e914
Eric Moore <EricGittaMoore@duck.com>
2022-08-20 23:43:39 +0800

    feat: 预览模块

---
+++ packages/preview/config/utils/index.ts
@@ -0,0 +1,7 @@
+export function isDev(mode: string): boolean {
+  return mode === 'development'
+}
+
+export function isProd(mode: string): boolean {
+  return mode === 'production'
+}
--- packages/preview/config/vite.config.base.ts
+++ packages/preview/config/vite.config.base.ts
@@ -1,51 +1,64 @@
 import { URL, fileURLToPath } from 'node:url'
+import * as fs from 'node:fs'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import Inspect from 'vite-plugin-inspect'
 import VueMarcos from 'unplugin-vue-macros/vite'
 import { previewOutput } from '@cow-low-code/build-utils'
 import configApiImport from './plugins/api-import'
 import configComponentsImport from './plugins/components-import'
-import configVisualizer from './plugins/visualizer'
 import configManualChunksPlugin from './plugins/manual-chunks'
+import type { ConfigEnv, UserConfig, UserConfigFn } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [
-    configManualChunksPlugin(),
-    vue(),
-    vueJsx(),
-    VueMarcos(),
-    configApiImport(),
-    configComponentsImport(),
-    Inspect(),
-    configVisualizer(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('../src', import.meta.url)),
-    },
-  },
-  build: {
-    outDir: previewOutput,
-    rollupOptions: {
-      output: {
-        manualChunks: (e) => {
-          if (
-            e.includes('/node_modules/vue/') ||
-            e.includes('/node_modules/vue-router/') ||
-            e.includes('/node_modules/pinia/') ||
-            e.includes('/node_modules/pinia-plugin-persist/') ||
-            e.includes('/node_modules/element-plus/') ||
-            e.includes('/node_modules/lodash-es/') ||
-            e.includes('/node_modules/@arco-design/web-vue/')
-          )
-            return 'vendor'
+export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
+  const isBuild = command === 'build'
+  return {
+    base: './',
+    plugins: [
+      configManualChunksPlugin(),
+      vue(),
+      vueJsx(),
+      VueMarcos(),
+      configApiImport(),
+      configComponentsImport(),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          /**
+           * webstorm无法识别导入
+           * 元素 'color-primary' 仅按名称解析，未使用显式导入
+           */
+          additionalData: [`@use "@/assets/style/global.scss" as *;`].join(''),
         },
       },
     },
-  },
-})
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('../src', import.meta.url)),
+      },
+    },
+    build: {
+      outDir: previewOutput,
+      rollupOptions: {
+        output: {
+          manualChunks: (e) => {
+            // fs.appendFile('./manual-chunks.txt', `${e}\n`, () => undefined)
+            if (
+              e.includes('/node_modules/vue/') ||
+              e.includes('/node_modules/vue-router/') ||
+              e.includes('/node_modules/pinia/') ||
+              e.includes('/node_modules/pinia-plugin-persist/') ||
+              e.includes('/node_modules/element-plus/') ||
+              e.includes('/node_modules/lodash-es/') ||
+              e.includes('/node_modules/@arco-design/web-vue/')
+            )
+              return 'vendor'
+          },
+        },
+      },
+    },
+  }
+}) as UserConfigFn
