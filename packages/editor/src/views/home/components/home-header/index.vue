3b5827f3ee457b703f826a84271ef7b4626352c6
JamesCurtis <49338067+james-curtis@users.noreply.github.com>
2022-08-21 21:58:31 +0800

    feat(editor): 撤销重做
    close #73

--- packages/editor/src/views/home/components/home-header/index.vue
+++ packages/editor/src/views/home/components/home-header/index.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="home-header">
     <el-row :style="{ height: props.styleHeaderHeight, 'line-height': props.styleHeaderHeight }">
+      <el-col :span="10">
+        <a-space class="edit-action-area" size="large">
+          <div class="edit-action">
+            <el-link :underline="false" title="撤销" @click="onUndo">
+              <icon-undo theme="outline" size="19" :stroke-width="3" />
+            </el-link>
+          </div>
+          <div class="edit-action">
+            <el-link :underline="false" title="重做" @click="onRedo">
+              <icon-redo theme="outline" size="19" :stroke-width="3" />
+            </el-link>
+          </div>
+        </a-space>
+      </el-col>
       <el-col :span="4">
         <div class="logo-wrapper">
-          <div class="logo" />
+          <a href="https://github.com/Cow-Coder/cow-Low-code" title="牛搭项目地址" target="_blank">
+            <img
+              src="@/assets/images/logo.svg"
+              alt="牛搭项目地址"
+              title="牛搭项目地址"
+              class="logo"
+              @dragstart.prevent
+            />
+          </a>
         </div>
       </el-col>
-      <el-col :span="10" />
       <el-col :span="10" class="justify-end flex items-center">
         <a-space size="large">
           <el-select
@@ -19,25 +40,23 @@
             <el-option v-for="val in presetList" :key="val.label" :value="val.label" />
           </el-select>
           <el-button type="danger" @click="handleResetAll">重置</el-button>
-          <el-button type="primary" @click="preview.onTogglePreviewDialog">预览</el-button>
+          <el-button type="primary" @click="onTogglePreviewDialog">预览</el-button>
         </a-space>
       </el-col>
     </el-row>
   </div>
-  <component :is="preview.previewComponent" />
+  <component :is="previewComponent" />
 </template>
 
 <script lang="ts" setup>
-import usePresetEvent from './use-preset-event'
-import type { Preset } from './type'
-import { useCodeStore } from '@/stores/code'
+import { Redo as IconRedo, Undo as IconUndo } from '@icon-park/vue-next'
+import usePreset from './use-preset'
 import usePreview from '@/views/home/components/home-header/use-preview'
+import useStateShot from '@/views/home/components/home-header/use-stateshot'
 
 defineOptions({
   name: 'HomeHeader',
 })
-
-const preview = usePreview()
 
 interface HeaderProps {
   styleHeaderHeight?: string
@@ -55,30 +74,25 @@ const handleResetAll = () => {
   emits('handleResetAll')
 }
 
-// 预设
-const codeStore = useCodeStore()
-const presetList = reactive<Preset[]>([usePresetEvent()])
-const choosePreset = ref<string>('')
-function onPresetSelectChange(val: string) {
-  if (val === '') return undefined
-  const found = presetList.find((e) => e.label === val)!
-  if (found.json === '') return undefined
-  new Promise(() => {
-    codeStore.jsonCode = JSON.parse(found.json)
-    ElMessage.success(`已加载: ${found.label}`)
-  })
-}
+const { undo: onUndo, redo: onRedo } = useStateShot()
+const { previewComponent, onTogglePreviewDialog } = usePreview()
+const { presetList, choosePreset, onPresetSelectChange } = usePreset()
 </script>
 
 <style lang="scss" scoped>
 .logo-wrapper {
-  @apply flex items-center;
+  @apply flex items-center justify-center;
   height: var(--style-header-height);
   line-height: var(--style-header-height);
   .logo {
-    @apply w-full;
     height: 50px;
-    background: url('/src/assets/images/logo.svg') no-repeat;
+    //background: url('/src/assets/images/logo.svg') center no-repeat;
+  }
+}
+
+.edit-action-area {
+  @apply flex items-center select-none;
+  .edit-action {
   }
 }
 </style>
