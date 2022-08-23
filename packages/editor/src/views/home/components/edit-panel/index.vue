56348e2942c9271a3023c06a5aaa14ca1b15e894
Eric Moore <EricGittaMoore@duck.com>
2022-08-23 13:53:05 +0800

    fix: 拖拽预览

--- packages/editor/src/views/home/components/edit-panel/index.vue
+++ packages/editor/src/views/home/components/edit-panel/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="edit">
+  <div ref="editCanvasRef" class="edit">
     <page-draggable
       :data-list="editableInstancedLibraryComponentData"
       :draggable-config="editDraggableConfigRef"
@@ -17,157 +17,30 @@
           <component :is="parseLibraryComponent(element)" />
         </div>
       </template>
-      <template v-if="draggedElement" #footer>
-        <preview-dragged :element="toRaw(draggedElement)" />
-      </template>
     </page-draggable>
   </div>
 </template>
 
 <script lang="tsx" setup>
-import { ref } from 'vue'
-import { cloneDeep, isEqual } from 'lodash-es'
-import { $$dropdown, DropdownOption, dispatchEventBatch } from '@cow-low-code/utils'
-import PreviewDragged from './components/preview-dragged'
-import type { ComponentPublicInstance, Ref } from 'vue'
-import type { Draggable } from '@/components/base-ui/kzy-draggable/types'
-import type { LibraryComponentInstanceData } from '@/types/library-component'
-import type { LibraryComponentInstanceEventTriggers } from '@/types/event-trigger'
-import type { getDefineComponent } from '@/utils/type'
-import { libraryMap } from '@/library'
-import { useCodeStore } from '@/stores/code'
+import useContentMenu from './use-content-menu'
+import useParseLibrary from './use-parse-library'
+import usePreventTouch from './use-prevent-touch'
+import useDragPreview from './use-drag-preview'
 import PageDraggable from '@/components/page-draggable/index.vue'
-import { CUSTOM_EVENT_TRIGGER_NAME, DRAGGABLE_GROUP_NAME } from '@/constant'
-import { isCustomEventTriggerName, uuid } from '@/utils/library'
 
 defineOptions({
   name: 'EditPanel',
 })
 
-const editDraggableConfigRef = ref<Draggable>({
-  draggableProp: {
-    group: { name: DRAGGABLE_GROUP_NAME },
-    itemKey: 'indexId',
-    disabled: false,
-    animation: 200,
-  },
-})
-
-const codeStore = useCodeStore()
-const componentRefMap = codeStore.componentRefMap
-const { jsonCode: editableInstancedLibraryComponentData, focusData } = storeToRefs(codeStore)
-
-// 根据名称解析物料组件库内的组件，这里没有注册全局组件是避免污染全局组件名称
-const libraryComponentPropTriggersCacheMap = new Map<
-  string,
-  LibraryComponentInstanceEventTriggers
->()
-
-function parseLibraryComponent(data: LibraryComponentInstanceData) {
-  const component = libraryMap[data.componentName]
-  if (!component) throw new Error(`library component: ${data.libraryName} not found`)
-  /*
-   * "props": {
-   *       "title": "按钮"
-   *     }
-   */
-  const props = cloneDeep(data.props)
-  if (
-    props &&
-    component.props &&
-    CUSTOM_EVENT_TRIGGER_NAME in component.props &&
-    data.eventTriggers
-  ) {
-    const customTriggers = Object.fromEntries(
-      Object.entries(data.eventTriggers).filter(([triggerName]) =>
-        isCustomEventTriggerName(triggerName)
-      )
-    )
-    props[CUSTOM_EVENT_TRIGGER_NAME] = customTriggers
-    if (!libraryComponentPropTriggersCacheMap.has(data.indexId))
-      libraryComponentPropTriggersCacheMap.set(data.indexId, customTriggers)
-    else {
-      if (!isEqual(libraryComponentPropTriggersCacheMap.get(data.indexId), customTriggers))
-        // 修改v-for的键值 强制重新渲染组件 重新执行setup
-        data.indexId = uuid()
-    }
-  }
-
-  function bindComponentRef(el: ComponentPublicInstance | null) {
-    if (!el && componentRefMap.has(data.uuid)) componentRefMap.delete(data.uuid)
-    else componentRefMap.set(data.uuid, el!)
-  }
-
-  return (
-    <component
-      ref={bindComponentRef}
-      onDispatchEvent={(eventTriggerName: string) =>
-        dispatchEventBatch(data, eventTriggerName, codeStore.jsonCode, componentRefMap)
-      }
-      {...props}
-    ></component>
-  )
-}
-
-const isDownSpace = ref(false)
-
-function onChoose(data: LibraryComponentInstanceData) {
-  isDownSpace.value || codeStore.dispatchFocus(data.uuid)
-}
-
-watch(isDownSpace, (val) => {
-  editDraggableConfigRef.value.draggableProp.disabled = val
-})
-
-function isFocusComponent(data: LibraryComponentInstanceData) {
-  return data.uuid == focusData.value?.uuid && !isDownSpace.value
-}
-
-function onTouchEvent(e: TouchEvent) {
-  if (!isDownSpace.value) e.stopPropagation()
-}
-
-// 右键菜单
-const onContextMenu = (e: MouseEvent, data: LibraryComponentInstanceData) => {
-  $$dropdown({
-    reference: e,
-    content: () => (
-      <>
-        <DropdownOption
-          label="删除节点"
-          icon="el-icon-delete"
-          {...{
-            onClick: () => {
-              codeStore.dispatchDelete(data.uuid)
-              // 删除节点后，取消选中
-              focusData.value = undefined
-              ElMessage.success('节点删除成功')
-            },
-          }}
-        />
-      </>
-    ),
-  })
-}
-
-useEventListener(window, 'keydown', (e) => {
-  if (
-    (e.target as HTMLElement).nodeName === 'INPUT' ||
-    (e.target as HTMLElement).nodeName === 'TEXTAREA'
-  )
-    return undefined
-  if (e.code === 'Space') {
-    isDownSpace.value = true
-    e.preventDefault()
-  }
-})
-useEventListener(window, 'keyup', (e) => {
-  e.code === 'Space' ? (isDownSpace.value = false) : undefined
-})
-
-// TODO: 拖拽到编辑器时候显示真实的组件，而不是显示物料面板的按钮
-const store = useCodeStore()
-const { draggedElement } = storeToRefs(store)
+const { isDownSpace, onTouchEvent, onChoose } = usePreventTouch()
+const { onContextMenu } = useContentMenu()
+const {
+  editDraggableConfigRef,
+  isFocusComponent,
+  parseLibraryComponent,
+  editableInstancedLibraryComponentData,
+} = useParseLibrary(isDownSpace)
+const { editCanvasRef } = useDragPreview()
 </script>
 
 <style lang="scss" scoped>
