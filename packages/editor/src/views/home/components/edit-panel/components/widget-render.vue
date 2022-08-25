afc52a855e10baa728419d9be75b4b04424a9a67
JamesCurtis <49338067+james-curtis@users.noreply.github.com>
2022-08-25 11:51:14 +0800

    chore: 待修复从根部替换jsonCode导致响应式全部失效

--- packages/editor/src/views/home/components/edit-panel/components/widget-render.vue
+++ packages/editor/src/views/home/components/edit-panel/components/widget-render.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="widget-render">
     <widget-item
+      v-if="true"
       v-bind="widgetProps"
       :ref="bindComponentRef"
       :comp-id="element.uuid"
@@ -13,12 +14,13 @@
         <slot :name="slotKey" />
       </template>
     </widget-item>
+    <component :is="renderFn" v-if="false" />
   </div>
 </template>
 
-<script lang="ts" setup>
+<script lang="tsx" setup>
 import type { ContainerMap, LibraryComponentInstanceData } from '@/types/library-component'
-import type { PropType } from 'vue'
+import type { ComputedRef, PropType, Ref } from 'vue'
 import useParseLibrary from '@/views/home/components/edit-panel/use-parse-library'
 
 defineOptions({ name: 'WidgetRender' })
@@ -38,7 +40,7 @@ const props = defineProps({
   },
 })
 const emits = defineEmits(['update:widgetElement'])
-const element = useVModel(props, 'widgetElement', emits)
+const element = useVModel(props, 'widgetElement', emits) as Ref<LibraryComponentInstanceData>
 
 const { parseLibraryComponent } = useParseLibrary(toRef(props, 'isDownSpace'))
 
@@ -46,6 +48,32 @@ const { parseLibraryComponent } = useParseLibrary(toRef(props, 'isDownSpace'))
 const { WidgetItem, widgetProps, handleDispatchEvent, bindComponentRef } = parseLibraryComponent(
   element.value
 )
+function renderFn() {
+  const widgetComponent = WidgetItem as unknown as JSX.Element
+  function dynamicSlotFn() {
+    const result = element.value.props?.slots
+      ? Object.entries(element.value.props?.slots as any).reduce((prev, cursor, index) => {
+          console.log(`element.value.props`, prev, cursor)
+          prev[cursor[0]] = () => <slot key={cursor[0]} v-slot={cursor[0]} />
+          return prev
+        }, {} as Record<string, any>)
+      : undefined
+    console.log(`result`, result, element.value.props?.slots)
+    return result
+  }
+  return (
+    <widgetComponent
+      {...widgetProps}
+      ref={bindComponentRef}
+      compId={element.value.uuid}
+      myInstanceData={element.value}
+      containerMap={props.containerMap}
+      onDispatchEvent={handleDispatchEvent}
+    >
+      {dynamicSlotFn()}
+    </widgetComponent>
+  )
+}
 
 // 拿到动态插槽的名字
 </script>
